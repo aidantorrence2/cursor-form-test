fd4bbdc8663dad742e2d2b3f1cd61ea684741212
Aidan Torrence <aidan.torrence@gmail.com>
2025-02-21 13:46:36 +1100

    CH-123

--- app/page.tsx
+++ app/page.tsx
@@ -192,7 +192,7 @@ export default function Home() {
                 </p>
                 <div className="text-center">
                   <Link
-                    href="https://www.instagram.com/direct/t/madebyaidan"
+                    href="https://www.instagram.com/m/madebyaidan"
                     target="_blank"
                     className="inline-flex items-center justify-center px-4 py-2 md:px-6 md:py-3 border border-transparent text-sm md:text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                   >
